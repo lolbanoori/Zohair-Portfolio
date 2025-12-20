0e6962e74500ab87d4f9c1bbfa65d10154879be0
Zohair Banoori <banoorizohair@gmail.com>
2025-12-20 23:43:04 +0500

    Update ScrollToTop.jsx
    Auto-scroll to top even worked on homepage, breaking use of Navbar

--- src/components/Utils/ScrollToTop.jsx
+++ src/components/Utils/ScrollToTop.jsx
@@ -2,11 +2,27 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
-    const { pathname } = useLocation();
+    const { pathname, hash } = useLocation();
 
     useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [pathname]);
+        // If there's no hash, standard "scroll to top" for new pages
+        if (!hash) {
+            window.scrollTo(0, 0);
+        }
+        // If there IS a hash (e.g. /#about), try to find the element and scroll
+        else {
+            // Small timeout to allow the DOM to mount/update before scrolling
+            const timer = setTimeout(() => {
+                const id = hash.replace('#', '');
+                const element = document.getElementById(id);
+                if (element) {
+                    element.scrollIntoView({ behavior: 'smooth' });
+                }
+            }, 100); // 100ms delay is usually sufficient for React to render
+
+            return () => clearTimeout(timer);
+        }
+    }, [pathname, hash]);
 
     return null;
 };
